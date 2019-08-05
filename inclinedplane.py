2d194ae3baa227ae7e7e51e0600d3ceaa7ae52a6
viktaur <30535579+viktaur@users.noreply.github.com>
2019-08-06 00:22:58 +0200

    Add files via upload

---
+++ inclinedplane.py
@@ -0,0 +1,194 @@
+from data import *
+import math
+import random
+import time
+import subprocess
+import platform
+
+def clear():
+    subprocess.Popen("cls" if platform.system() == "Windows" else "clear", shell=True)
+
+
+# INITIAL SCRIPT #
+def initial():
+    clear()
+    time.sleep(0.01)
+    print(Cyan + ' ')
+    print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
+    print('* I N C L I N E D   P L A N E   C A L C U L A T O R *')
+    print('* * * * * * * * * * * * * * * * * * * * * * * * * * *')
+    time.sleep(1)
+    print(' ')
+    time.sleep(0.1)
+    print(' ')
+    time.sleep(0.1)
+    print('                                                    *')
+    time.sleep(0.1)
+    print('                                                 ****')
+    time.sleep(0.1)
+    print('                                              *******')
+    time.sleep(0.1)
+    print('                                           **********')
+    time.sleep(0.1)
+    print('                                         ************')
+    time.sleep(0.1)
+    print('                                      ***************')
+    time.sleep(0.1)
+    print('                                   ******************')
+    time.sleep(0.1)
+    print('                                *********************')
+    time.sleep(0.1)
+    print('                             ************************')
+    time.sleep(0.1)
+    print('                          ***************************')
+    time.sleep(0.1)
+    print('                       ******************************')
+    time.sleep(0.1)
+    print('                    *********************************')
+    time.sleep(0.1)
+    print('                 ************************************')
+    time.sleep(0.1)
+    print('              ***************************************')
+    time.sleep(0.1)
+    print('           ******************************************')
+    time.sleep(0.1)
+    print('        *********************************************')
+    time.sleep(0.1)
+    print('     ************************************************')
+    time.sleep(0.1)
+    print('  ***************************************************')
+    time.sleep(0.1)
+    print(' ')
+    time.sleep(0.1)
+    print(' ')
+    time.sleep(0.1)
+    print(' ')
+    time.sleep(0.1)
+    print(' ' + Color_Off)
+    time.sleep(1)
+    print(Cyan + 'Press "S" to start calculator, "H" for help or information, "M" to return to the main menu or "E" to exit the program. Then press enter.' + Color_Off)
+
+    return
+
+def home():
+    # print(Cyan + 'Press "S" to start calculator, "H" for help or information, "M" to return to the main menu or "E" to exit the program. Then press enter.' + Color_Off)
+    home.varinput = input()
+
+    if home.varinput in ['S', 's', 'START', 'start']:
+        # INPUT SCRIPT #
+
+        def ObjectMass():
+            mNoEval = input().lower()
+            try:
+                m = eval(mNoEval)
+            except NameError:
+                print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+                time.sleep(0.01)
+                ObjectMass()
+            return m
+
+        print(Cyan + 'Insert object mass (kg): ' + Color_Off)
+        ObjectMass()
+
+
+
+        def ObjectGravity():
+            gNoEval = input().lower()
+            try:
+                g = eval(gNoEval)
+            except NameError:
+                print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+                time.sleep(0.01)
+                ObjectGravity()
+            return g
+
+        print(Cyan + 'Insert gravity (m/s²): ' + Color_Off)
+        ObjectGravity()
+
+
+
+        def ObjectInclination():
+            ÂNoEval = input().lower()
+            try:
+                Â = eval(ÂNoEval)
+            except NameError:
+                print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+                time.sleep(0.01)
+                ObjectInclination()
+            return Â
+
+
+        print(Cyan + 'Insert plane\'s inclination angel (DEG): ' + Color_Off)
+        ObjectInclination()
+
+
+
+        def ObjectFrictionCoef():
+            µNoEval = input().lower()
+            try:
+                µ = eval(µNoEval)
+            except NameError:
+                print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+                time.sleep(0.01)
+                ObjectFrictionCoef()
+            return µ
+
+        print(Cyan + 'Insert coefficient of friction: ' + Color_Off)
+        ObjectFrictionCoef()
+
+
+
+        # CALCULATIONS AND FORMULES #
+        P = m * g
+        Px = m * g * math.sin(math.radians(Â))
+        Py = m * g * math.cos(math.radians(Â))
+        N = -Py
+        Fƒ = µ * N
+        Fres = Px + Fƒ
+        a = Fres / m
+
+        # DISPLAY SCRIPT #
+        print(' ')
+        print(Cyan + 'INPUT' + Color_Off)
+        print('m = ' + str(m) + ' kg')
+        print('g = ' + str(g) + ' m/s²')
+        print('Â = ' + str(Â) + 'º')
+        print('µ = ' + str(µ))
+        print(' ')
+        print(Cyan + 'OUTPUT' + Color_Off)
+        print('P = ' + str(P) + ' N')
+        print('Px = ' + str(Px) + ' N')
+        print('Py = ' + str(Py) + ' N')
+        print('N = ' + str(N) + ' N')
+        print('Fƒ = ' + str(Fƒ) + ' N')
+        print('Fres = ' + str(Fres) + ' N')
+        print('a (Fres) = ' + str(a) + ' m/s²')
+        time.sleep(3)
+        print(' ')
+        print(' ')
+        print(Cyan + 'Press "S" to start calculator, "H" for help or information, "M" to return to the main menu or "E" to exit the program. Then press enter.' + Color_Off)
+        home()
+        return
+
+
+    elif home.varinput in ['H', 'h', 'HELP', 'help', 'INFO', 'info', 'I', 'i']:
+        from physics import info
+
+    elif home.varinput in ['E', 'e', 'EXIT', 'exit']:
+        print('Leaving program')
+
+    elif home.varinput in ['M', 'm', 'MENU', 'menu']:
+        import physics
+
+    else:
+        print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+        home()
+
+    return
+
+# while True:
+#     initial()
+#     home()
+
+initial()
+home()
---
+++ physics.py
@@ -0,0 +1,85 @@
+from data import *
+import math
+import random
+import time
+import subprocess
+import platform
+
+def clear():
+    subprocess.Popen("cls" if platform.system() == "Windows" else "clear", shell=True)
+
+def info():
+    clear()
+    time.sleep(0.01)
+    print(UCyan + 'What is YuCode Dev Physics Calculator?' + Color_Off)
+    print('This project contains some physics calculators like inclined-plane or gravitation. In them, some input (like a mass) is introduced and some output (weight, friction force, orbital velocity...) is expected.' + Color_Off)
+    print(UCyan + 'Data variables' + Color_Off)
+    print('Some data like Earth\'s mass or the radius of Mass\' orbit is stored in this program. Here are the available variables so far:')
+    print('- average_person_mass')
+    print('- earth_gravity')
+    print('- earth_mass')
+    print('- earth_moon_distance')
+    print('- earth_radius')
+    print('- earth_sun_distance')
+    print('- G (gravitational_constant)')
+    print('- jupiter_mass')
+    print('- mars_mass')
+    print('- mars_sun_distance')
+    print('- mercury_mass')
+    print('- mercury_sun_distance')
+    print('- moon_mass')
+    print('- neptune_mass')
+    print('- saturn_mass')
+    print('- uranus_mass')
+    print('- venus_mass')
+    print('- venus_sun_distance')
+
+
+    input(Green + "Press any key to continue..." + Color_Off)
+    clear()
+    time.sleep(0.01)
+    program_start()
+    return
+
+
+def input_loop():
+    ProgramNumber = input()
+
+    if ProgramNumber in ['E', 'e', 'EXIT', 'exit']:
+        print(Red + 'Leaving program' + Color_Off)
+
+    elif ProgramNumber == str(0):
+        info()
+
+    elif ProgramNumber == str(1):
+        import inclinedplane
+
+    elif ProgramNumber == str(2):
+        import gravitation
+
+    else:
+        print(Red + 'ERROR: Unexpected input! Please try again:' + Color_Off)
+        input_loop()
+
+    return
+
+
+def program_start():
+    print(' ')
+    print(Yellow + 'Which program would you like to start? You should read the INFO before starting the program for the first time.')
+    print(' ' + Color_Off)
+    print(Red + '[E] EXIT' + Color_Off)
+    print(Green + '[0] INFO' + Color_Off)
+    print(Yellow + '[1] Inclined plane' + Color_Off)
+    print(Yellow + '[2] Gravitation' + Color_Off)
+    print(' ')
+    input_loop()
+    return
+
+# time.sleep(1)
+clear()
+time.sleep(0.01)
+print(Yellow + 'WELCOME TO YUCODE DEV PHYSICS CALCULATOR!')
+time.sleep(1)
+
+program_start()
