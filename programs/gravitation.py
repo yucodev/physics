1b53a68a1c89c4bf144a89eb27da946654a8e185
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2019-09-04 21:42:38 +0200

    Update gravitation.py

--- programs/gravitation.py
+++ programs/gravitation.py
@@ -1,24 +1,31 @@
-from datafiles.data import *
 import math
 import random
 import time
-import subprocess
 import platform
 
-# Gravitation Calculator Script (Execute This Script in Python 3)
-
-
-def clear():
-    subprocess.Popen("cls" if platform.system() == "Windows" else "clear", shell=True)
-
+average_person_mass = 70
+earth_gravity = 9.80665
+earth_mass = 5.97e+24
+earth_moon_distance = 3.844e+8
+earth_radius = 6371000
+earth_sun_distance = 1.496e+11
+G = 6.67e-11
+jupiter_mass = 1.898e+27
+mars_mass = 6.39e+23
+mars_sun_distance = 227.9e+6
+mercury_mass = 3.285e+23
+mercury_sun_distance = 57.91e+6
+moon_mass = 7.34e+22
+neptune_mass = 1.024e+26
+saturn_mass = 5.683e+26
+uranus_mass = 8.681e+25
+venus_mass = 4.867e+24
+venus_sun_distance = 108.2e+6
 
 # INITIAL SCRIPT #
 def initial():
-    clear()
-    print(Cyan + ' ')
-    print('* * * * * * * * * * * * * * * * * * * * * * * *')
-    print('* G R A V I T A T I O N   C A L C U L A T O R *')
-    print('* * * * * * * * * * * * * * * * * * * * * * * *')
+    print('----G R A V I T A T I O N----')
+    print('---- C A L C U L A T O R ---- ')
     time.sleep(1)
     print(' ')
     time.sleep(0.1)
@@ -35,44 +42,22 @@ def initial():
     print('           *         *   *              *      ')
     time.sleep(0.1)
     print('                                               ')
-    time.sleep(0.1)
-    print('                                               ')
-    time.sleep(0.1)
-    print('                     ****                      ')
-    time.sleep(0.1)
-    print('                   ********                    ')
-    time.sleep(0.1)
-    print('                  **********                   ')
-    time.sleep(0.1)
-    print('      (________  ************  _________)      ')
-    time.sleep(0.1)
-    print('      (________  ************  _________)      ')
-    time.sleep(0.1)
-    print('                  **********                   ')
-    time.sleep(0.1)
-    print('                   ********                    ')
-    time.sleep(0.1)
-    print('                     ****                      ')
-    time.sleep(0.1)
-    print(' ')
-    time.sleep(0.1)
-    print(' ' + Color_Off)
     time.sleep(1)
 
 
 def home():
-    print(Cyan + 'Press "S" to start calculator, "H" for help or information, "M" to return to the main menu or "E" to exit the program. Then press enter.' + Color_Off)
+    print('Press "S" to start calculator or "E" to exit the program. Then press enter.')
     home.varinput = input()
 
     if home.varinput in ['S', 's', 'START', 'start']:
 
         # INPUT SCRIPT #
-        print(Cyan + 'Insert the bigger mass (kg): ' + Color_Off)
-        M = eval(input())
-        print(Cyan + 'Insert the smaller mass (kg): ' + Color_Off)
-        m = eval(input())
-        print(Cyan + 'Insert the distance of the objects or the radius of the orbit (m): ' + Color_Off)
-        R = eval(input())
+        print('Insert the bigger mass (kg): ')
+        M = int(input())
+        print('Insert the smaller mass (kg): ')
+        m = int(input())
+        print('Insert the distance of the objects or the radius of the orbit (m): ')
+        R = int(input())
 
         # CALCULATIONS AND FORMULES #
         g = G * M / (R**2)
@@ -81,42 +66,38 @@ def home():
         P2 = M * a * (-1)
         v = math.sqrt(G * M / R)
         T = (2 * math.pi * R) / v
-        ω = v / R     # or (2 * math.pi) / T
+        w = v / R     # or (2 * math.pi) / T
 
         # DISPLAY SCRIPT #
         print(' ')
-        print(Cyan + 'INPUT' + Color_Off)
+        print('INPUT')
         print('M = ' + str(M) + ' kg')
         print('m = ' + str(m) + ' kg')
         print('R = ' + str(R) + ' m')
         print(' ')
-        print(Cyan + 'OUTPUT' + Color_Off)
+        print('OUTPUT')
         print('P (M on m) = ' + str(P) + ' N')
         print('P (m on M) = ' + str(P2) + ' N')
         print('g = ' + str(g) + ' m/s2')
         print('a = ' + str(a) + ' m/s2')
         print('T = ' + str(T) + ' s')
         print('v = ' + str(v) + ' m/s')
-        print('ω = ' + str(ω) + ' rad/s')
+        print('w = ' + str(w) + ' rad/s')
         time.sleep(3)
-        print(' ')
         print(' ')
         home()
 
     elif home.varinput in ['H', 'h', 'HELP', 'help', 'INFO', 'info', 'I', 'i']:
-        from start import info
+        from physics import info
 
     elif home.varinput in ['E', 'e', 'EXIT', 'exit']:
         print('Leaving program')
 
-    elif home.varinput in ['M', 'm', 'MENU', 'menu']:
-        import start
-        # from start import *
-
     else:
-        print(Red + 'Error, type a valid input' + Color_Off)
+        print('Error, type a valid input')
         home()
 
+    return
 
 initial()
 home()
